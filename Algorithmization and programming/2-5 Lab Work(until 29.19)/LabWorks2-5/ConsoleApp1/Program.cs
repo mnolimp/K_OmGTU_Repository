1d5fae3864680ad6534b34882efb3355468a435f
mnolimp <37767648+mnolimp@users.noreply.github.com>
2023-10-20 20:58:12 +0600

    Lab Works 2-5

---
+++ Algorithmization and programming/2-5 Lab Work(until 29.19)/LabWorks2-5/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿int y, a, b, x1, x2; //y - результат вычислений; a, b, x1, x2 - подаваемые на вход значения переменных
+
+Console.WriteLine("Программа для вычисления значения формулы:");
+Console.WriteLine("          a^2");
+Console.WriteLine("y = --------------- + ln(a + bx)");
+Console.WriteLine("    (x + 2)e^(-bx)");
