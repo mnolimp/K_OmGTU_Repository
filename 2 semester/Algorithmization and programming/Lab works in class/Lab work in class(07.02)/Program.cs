aec815df9a55ea7fedf88064025b73ca4f16788b
mnolimp <37767648+mnolimp@users.noreply.github.com>
2024-02-09 09:30:13 +0600

    Patch(try/catch)

--- 2 semester/Algorithmization and programming/Lab works in class/Lab work in class(07.02)/Program.cs
+++ 2 semester/Algorithmization and programming/Lab works in class/Lab work in class(07.02)/Program.cs
@@ -10,17 +10,26 @@
         }
 
 
-        class Menu
+        public class Menu
         {
             public Menu()
             {
 
             }
 
+            public static void showIfDidntFind()
+            {
+                Console.Clear();
+                Console.WriteLine("Такой аудитории не было найдено\nНажмите любую клавишу, чтобы вернуться в меню");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
             public void showMenu()
             {
                 do
                 {
+                    Console.Clear();
                     int variant;
                     Console.WriteLine("1.  Создание бд\r\n2.  Добавление в базу\r\n3.  Изменение данных аудитории по заданному номеру\r\n4.  Выборка аудиторий с кол-вом посадочных мест больше равным заданного \r\n5.  Выборка аудиторий с проектором \r\n6.  Выборка аудиторий с пк и заданным кол-вом посадочных мест\r\n7.  Выборка аудиторий по номеру этажа\r\n8.  Вывод всех данных по аудиториям\r\n9.  Выход");
                     variant = Convert.ToInt32(Console.ReadLine());
@@ -34,11 +43,25 @@
                             break;
                         case 3:
                             Console.WriteLine("Напишите номер аудитории в формате <<этаж-номер>>");
-                            Auditorium.changeInfobyNumber(Console.ReadLine());
+                            try
+                            {
+                                Auditorium.changeInfobyNumber(Console.ReadLine());
+                            } catch (System.FormatException)
+                            {
+                                Console.WriteLine("Данные введены некорректно. Операция прервана. Нажмите любую клавишу, чтобы вернуться в меню");
+                                Console.ReadKey();
+                            }
                             break;
                         case 4:
                             Console.WriteLine("Введите число посадочных мест");
-                            Auditorium.findAuditoriumswithPlaces(Convert.ToInt32(Console.ReadLine()));
+                            try
+                            {
+                                Auditorium.findAuditoriumswithPlaces(Convert.ToInt32(Console.ReadLine()));
+                            } catch (System.FormatException)
+                            {
+                                Console.WriteLine("Данные введены некорректно. Операция прервана. Нажмите любую клавишу, чтобы вернуться в меню");
+                                Console.ReadKey();
+                            }
                             break;
                         case 5:
                             Auditorium.findAuditoriumwithProjector();
@@ -49,7 +72,14 @@
                             places = Convert.ToInt32(Console.ReadLine());
                             Console.WriteLine("Введите количество ПК");
                             PCs = Convert.ToInt32(Console.ReadLine());
-                            Auditorium.findAuditoriumwithPCsAndPlaces(places, PCs);
+                            try
+                            {
+                                Auditorium.findAuditoriumwithPCsAndPlaces(places, PCs);
+                            } catch (System.FormatException)
+                            {
+                                Console.WriteLine("Данные введены некорректно. Операция прервана. Нажмите любую клавишу, чтобы вернуться в меню");
+                                Console.ReadKey();
+                            }
                             break;
                         case 7:
                             Auditorium.findAuditoriumbyFlat(0);
@@ -95,68 +125,13 @@
                 int places;
                 bool projector;
                 int PCs;
-
-                Console.WriteLine("Введите номер аудитории в формате <<этаж-номер>>");
-                number = Console.ReadLine();
-
-                Console.WriteLine("Введите количество посадочных мест");
-                places = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Подтвердите наличие/отсуствие проектора в аудитории(Да/Нет)");
-                strProjector = Console.ReadLine();
-
-                switch (strProjector)
+                try
                 {
-                    case "да":
-                    case "Да":
-                    case "ДА":
-                    case "дА":
-                        projector = true;
-                        break;
-                    case "нет":
-                    case "Нет":
-                    case "НЕТ":
-                    case "нЕТ":
-                    case "нЕт":
-                    case "неТ":
-                    case "НеТ":
-                    case "НЕт":
-                        projector = false;
-                        break;
-                    default:
-                        projector = false;
-                        break;
-                }
-
-                Console.WriteLine("Введите количество компьютеров в аудитории(введите 0 если их нет)");
-                PCs = Convert.ToInt32(Console.ReadLine());
-
-                auditoriums.Add(new Auditorium(number, places, projector, PCs));
-
-                Console.Clear();
-                Console.WriteLine("Аудитория успешно добавлена в базу данных \nНажмите любую клавишу, чтобы вернуться в меню");
-                Console.ReadKey();
-                Console.Clear();
-            }
-
-            public static void changeInfobyNumber(String number)
-            {
-                Auditorium changingAuditorium = findAuditoriumbyNumber(number);
-                if (changingAuditorium == null) 
-                {
-                    Console.WriteLine("Такой аудитории не было найдено");
-                } else
-                {
-                    String changingNumber, strProjector;
-                    int changingPlaces, indexOfChangingAuditorium;
-                    bool changingProjector;
-                    int changingPCs;
-
                     Console.WriteLine("Введите номер аудитории в формате <<этаж-номер>>");
-                    changingNumber = Console.ReadLine();
+                    number = Console.ReadLine();
 
                     Console.WriteLine("Введите количество посадочных мест");
-                    changingPlaces = Convert.ToInt32(Console.ReadLine());
+                    places = Convert.ToInt32(Console.ReadLine());
 
                     Console.WriteLine("Подтвердите наличие/отсуствие проектора в аудитории(Да/Нет)");
                     strProjector = Console.ReadLine();
@@ -167,7 +142,7 @@
                         case "Да":
                         case "ДА":
                         case "дА":
-                            changingProjector = true;
+                            projector = true;
                             break;
                         case "нет":
                         case "Нет":
@@ -177,67 +152,199 @@
                         case "неТ":
                         case "НеТ":
                         case "НЕт":
-                            changingProjector = false;
+                            projector = false;
                             break;
                         default:
-                            changingProjector = false;
+                            projector = false;
                             break;
                     }
 
                     Console.WriteLine("Введите количество компьютеров в аудитории(введите 0 если их нет)");
-                    changingPCs = Convert.ToInt32(Console.ReadLine());
+                    PCs = Convert.ToInt32(Console.ReadLine());
+                    if (auditoriums != null)
+                    {
+                        auditoriums.Add(new Auditorium(number, places, projector, PCs));
+                        Console.Clear();
+                        Console.WriteLine("Аудитория успешно добавлена в базу данных \nНажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                        Console.Clear();
 
-                    Auditorium updatedAuditorium = new Auditorium(changingNumber, changingPlaces, changingProjector, changingPCs);
-                    indexOfChangingAuditorium = auditoriums.IndexOf(changingAuditorium);
-                    auditoriums[indexOfChangingAuditorium] = updatedAuditorium;
+                    }
+                    else
+                    {
+                        Console.WriteLine("База данных не инициализированна. Добавление аудитории в базу данных прервано. Нажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                    }
+                } catch (System.FormatException)
+                {
 
-                    Console.Clear();
-                    Console.WriteLine("Аудитория успешно отредактирована \nНажмите любую клавишу, чтобы вернуться в меню");
+                    Console.WriteLine("Данные введены некорректно. Добавление аудитории в базу данных прервано. Нажмите любую клавишу, чтобы вернуться в меню");
                     Console.ReadKey();
-                    Console.Clear();
+                }
+            }
+
+
+            public static void changeInfobyNumber(String number)
+            {
+                if (auditoriums != null)
+                {
+                    Auditorium changingAuditorium = findAuditoriumbyNumber(number);
+                    if (changingAuditorium == null)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Такой аудитории не было найдено\nНажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                        Console.Clear();
+                        
+                    }
+                    else
+                    {
+                        String changingNumber, strProjector;
+                        int changingPlaces, indexOfChangingAuditorium;
+                        bool changingProjector;
+                        int changingPCs;
+
+                        Console.WriteLine("Введите номер аудитории в формате <<этаж-номер>>");
+                        changingNumber = Console.ReadLine();
+
+                        Console.WriteLine("Введите количество посадочных мест");
+                        changingPlaces = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Подтвердите наличие/отсуствие проектора в аудитории(Да/Нет)");
+                        strProjector = Console.ReadLine();
+
+                        switch (strProjector)
+                        {
+                            case "да":
+                            case "Да":
+                            case "ДА":
+                            case "дА":
+                                changingProjector = true;
+                                break;
+                            case "нет":
+                            case "Нет":
+                            case "НЕТ":
+                            case "нЕТ":
+                            case "нЕт":
+                            case "неТ":
+                            case "НеТ":
+                            case "НЕт":
+                                changingProjector = false;
+                                break;
+                            default:
+                                changingProjector = false;
+                                break;
+                        }
+
+                        Console.WriteLine("Введите количество компьютеров в аудитории(введите 0 если их нет)");
+                        changingPCs = Convert.ToInt32(Console.ReadLine());
+
+                        Auditorium updatedAuditorium = new Auditorium(changingNumber, changingPlaces, changingProjector, changingPCs);
+                        indexOfChangingAuditorium = auditoriums.IndexOf(changingAuditorium);
+                        auditoriums[indexOfChangingAuditorium] = updatedAuditorium;
+
+                        Console.Clear();
+                        Console.WriteLine("Аудитория успешно отредактирована \nНажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("База данных не инициализированна. Операция прервана. Нажмите любую клавишу, чтобы вернуться в меню");
+                    Console.ReadKey();
                 }
             }
 
             public static void findAuditoriumswithPlaces(int places)
             {
-                for(int i = 0; i < auditoriums.Count; i++)
+                Console.Clear();
+                bool foundAuditorium = false;
+                if (auditoriums != null)
                 {
-                    if (auditoriums[i].places >= places)
+                    for (int i = 0; i < auditoriums.Count; i++)
                     {
-                        Console.WriteLine(getAllInfo(auditoriums[i]));
+                        if (auditoriums[i].places >= places)
+                        {
+                            Console.WriteLine(getAllInfo(auditoriums[i]));
+                            foundAuditorium = true;
+                        }
+                    }
+                    if (foundAuditorium)
+                    {
+                        Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                        Console.Clear();
+                    } else
+                    {
+                        Menu.showIfDidntFind();
                     }
                 }
-                Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
-                Console.ReadKey();
-                Console.Clear();
+                else
+                {
+                    Console.WriteLine("База данных не инициализированна. Операция прервана. Нажмите любую клавишу, чтобы вернуться в меню");
+                    Console.ReadKey();
+                }
             }
 
             public static void findAuditoriumwithProjector()
             {
-                for(int i = 0; i < auditoriums.Count; i++)
-                {
-                    if (auditoriums[i].projector == true)
-                    {
-                        Console.WriteLine(getAllInfo(auditoriums[i]));
-                    }
-                }
-                Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
-                Console.ReadKey();
                 Console.Clear();
+                bool foundAuditorium = false;
+                if (auditoriums != null)
+                {
+                    for (int i = 0; i < auditoriums.Count; i++)
+                    {
+                        if (auditoriums[i].projector == true)
+                        {
+                            Console.WriteLine(getAllInfo(auditoriums[i]));
+                            foundAuditorium = true;
+                        }
+                    }
+                    if (foundAuditorium)
+                    {
+                        Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                        Console.Clear();
+                    } else
+                    {
+                        Menu.showIfDidntFind();
+                    }
+                } else
+                {
+                    Console.WriteLine("База данных не инициализированна. Операция прервана. Нажмите любую клавишу, чтобы вернуться в меню");
+                    Console.ReadKey();
+                }
             }
 
             public static void findAuditoriumwithPCsAndPlaces(int places, int PCs)
             {
-                for(int i = 0; i < auditoriums.Count; i++)
-                {
-                    if ((auditoriums[i].places >= places) & (auditoriums[i].PCs >= PCs))
-                    {
-                        Console.WriteLine(getAllInfo(auditoriums[i]));
-                    }
-                }
-                Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
-                Console.ReadKey();
                 Console.Clear();
+                bool foundAuditorium = false;
+                if (auditoriums != null)
+                {
+                    for (int i = 0; i < auditoriums.Count; i++)
+                    {
+                        if ((auditoriums[i].places >= places) & (auditoriums[i].PCs >= PCs))
+                        {
+                            Console.WriteLine(getAllInfo(auditoriums[i]));
+                            foundAuditorium = true;
+                        }
+                    }
+                    if (foundAuditorium)
+                    {
+                        Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                        Console.Clear();
+                    } else
+                    {
+                        Menu.showIfDidntFind();
+                    }
+                } else
+                {
+                    Console.WriteLine("База данных не инициализированна. Операция прервана. Нажмите любую клавишу, чтобы вернуться в меню");
+                    Console.ReadKey();
+                }
             }
 
             public static void findAuditoriumbyFlat(int flat)
@@ -249,13 +356,32 @@
 
             public static void getAllInfoAllAuditoriums()
             {
-                for(int i = 0; i < auditoriums.Count(); i++)
-                {
-                    Console.WriteLine(getAllInfo(auditoriums[i]));
-                }
-                Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
-                Console.ReadKey();
+                bool foundAuditorium = false;
                 Console.Clear();
+                if (auditoriums != null)
+                {
+                    for (int i = 0; i < auditoriums.Count(); i++)
+                    {
+                        Console.WriteLine(getAllInfo(auditoriums[i]));
+                        foundAuditorium = true;
+                    }
+                    if (foundAuditorium)
+                    {
+                        Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                        Console.Clear();
+                    } else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("База данных пуста\nНажмите любую клавишу, чтобы вернуться в меню");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                } else
+                {
+                    Console.WriteLine("База данных не инициализированна. Операция прервана. Нажмите любую клавишу, чтобы вернуться в меню");
+                    Console.ReadKey();
+                }
             }
 
             public void setNumber(Auditorium auditorium, String number)
